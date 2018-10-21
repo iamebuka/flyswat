92f5009826b5f4a2aa05d01cfc000ed8320abfc4
iamebuka <obukweluebuka@gmail.com>
2018-10-21 17:05:00 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express()
+const fs = require('fs');
+const server = require("http").createServer(app)
+
+
+app.use("/static", express.static(__dirname + "/public"))
+
+let readfile = (path) => { 
+    
+    
+}
+app.get("/h", (req, res) => {
+
+    res.sendFile(__dirname + "/public/view/index.html")
+})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/view/default.html")
+})
+server.listen(8080, () => console.log("listening on port 8080"))
