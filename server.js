4b03c6bdefaba405fe622fbc170321a5b6d7c670
iamebuka <obukweluebuka@gmail.com>
2018-10-21 17:32:15 +0100

    changed port from 3000 to 5000 and to include env.PORT because of the error i am recieving while deploying about not able to bind port within 60 seconds of launch

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express()
 const fs = require('fs');
-const server = require("http").createServer(app)
-
 
 app.use("/static", express.static(__dirname + "/public"))
 
-let readfile = (path) => { 
-    
-    
-}
+
 app.get("/h", (req, res) => {
 
     res.sendFile(__dirname + "/public/view/index.html")
@@ -17,4 +12,4 @@ app.get("/h", (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/view/default.html")
 })
-server.listen(3000, () => console.log("listening on port 3000"))
+app.listen(process.env.PORT || 5000, () => console.log("listening on port 5000"))
